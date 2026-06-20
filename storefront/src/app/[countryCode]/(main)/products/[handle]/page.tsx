b3c72468e989c23d7e8b3ee567c6f90812fcf0b1
bapan470 <84938243+bapan470@users.noreply.github.com>
2026-06-21 01:25:22 +0530

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
-import { getRegion } from "@lib/data/regions"
+
+import { sdk } from "@lib/config"
+import { getRegion, listRegions } from "@lib/data/regions"
 import {
   getProductByHandle,
   getProductFashionDataByHandle,
@@ -11,10 +13,44 @@ type Props = {
   params: Promise<{ countryCode: string; handle: string }>
 }
 
-export const dynamicParams = true
-
 export async function generateStaticParams() {
-  return []
+  try {
+    const countryCodes = await listRegions().then(
+      (regions) =>
+        regions
+          ?.map((r) => r.countries?.map((c) => c.iso_2))
+          .flat()
+          .filter(Boolean) as string[]
+    )
+
+    if (!countryCodes) {
+      return []
+    }
+
+    const { products } = await sdk.store.product.list(
+      { fields: "handle" },
+      { next: { tags: ["products"] } }
+    )
+
+    const staticParams = countryCodes
+      ?.map((countryCode) =>
+        products.map((product) => ({
+          countryCode,
+          handle: product.handle,
+        }))
+      )
+      .flat()
+      .filter((product) => product.handle)
+
+    return staticParams
+  } catch (error) {
+    console.error(
+      `Failed to generate static paths for product pages: ${
+        error instanceof Error ? error.message : "Unknown error"
+      }.`
+    )
+    return []
+  }
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
@@ -68,3 +104,5 @@ export default async function ProductPage({ params }: Props) {
     />
   )
 }
+
+
